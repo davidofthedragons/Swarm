8fac413d29aa8ad87b58bd4bbb7c7fdaf0ee9d6d
David Gardner <davidofthedragons@gmail.com>
2013-03-30 21:32:24 -0400

    Added slightly glitchy and incomplete GUI

--- src/swarm/Swarm.java
+++ src/swarm/Swarm.java
@@ -1,18 +1,22 @@
 package swarm;
 
 import java.awt.*;
+import java.awt.event.*;
 import java.util.Random;
 
 import javax.swing.*;
 
-public class Swarm extends JFrame {
+public class Swarm extends JFrame implements ActionListener {
 
-	private static final int FLIES = 53;
-	private static final boolean DIAGONALS = true;
+	private static int FLIES = 1000;
+	private static boolean DIAGONALS = false;
 	private static final int SX = 600;
 	private static final int SY = 600;
-	private static final int DELAY = 100;
-	private static final int ADIST = 0; //Acceptable distance
+	private static int DELAY = 100;
+	private static int ADIST = 0; //Acceptable distance
+	private static int speed = 5;
+	private static boolean cls = false;
+	private static boolean RUNNING = false;
 	private Random rand = new Random(53);
 	/*   405
 	 *   \|/
@@ -26,19 +30,18 @@ public class Swarm extends JFrame {
 	public Swarm() {
 		super("Swarm Intelligence");
 		setSize(600, 600);
-		
 		flies = new Fly[FLIES];
-		
 		reset();
 		createGUI();
-		
 		Thread updater = new Thread(new Runnable() {
 			public void run() {
-				while(true) {
-					for(int i=0; i<FLIES; i++) {
-						flies[i].update();
+				while (true) {
+					if (RUNNING) {
+						for (int i = 0; i < FLIES; i++) {
+							flies[i].update();
+						}
+						swarmPanel.repaint();
 					}
-					swarmPanel.repaint();
 					try {
 						Thread.sleep(DELAY);
 					} catch (InterruptedException e) {
@@ -58,11 +61,58 @@ public class Swarm extends JFrame {
 			flies[i] = new Fly(SX/2, SY/2, i);
 			//flies[i].setPos(0, 0);
 		}
-		flies[0].setPos(300, 301);
-		flies[1].setPos(300, 300);
+		//flies[0].setPos(300, 301);
+		//flies[1].setPos(300, 300);
 	}
 	public void createGUI() {
 		JPanel optionPanel = new JPanel();
+		JLabel fLabel = new JLabel("Flies:");
+		optionPanel.add(fLabel);
+		final JTextField flyField = new JTextField(5);
+		flyField.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				FLIES = Integer.parseInt(flyField.getText());
+			}
+		});
+		optionPanel.add(flyField);
+		final JCheckBox diagBox = new JCheckBox("Diagonals");
+		diagBox.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				DIAGONALS = diagBox.isSelected();
+			}
+		});
+		optionPanel.add(diagBox);
+		JLabel adLabel = new JLabel("Range:");
+		optionPanel.add(adLabel);
+		final JTextField adField = new JTextField(5);
+		adField.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				ADIST = Integer.parseInt(adField.getText());
+			}
+		});
+		optionPanel.add(adField);
+		final JCheckBox clsBox = new JCheckBox("Clear Screen");
+		clsBox.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				cls = clsBox.isSelected();
+			}
+		});
+		optionPanel.add(clsBox);
+		final JButton stspButton = new JButton("Start");
+		stspButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(RUNNING) {
+					RUNNING = false;
+					stspButton.setText("Start");
+				}
+				else {
+					RUNNING = true;
+					stspButton.setText("Stop");
+				}
+			}
+		});
+		optionPanel.add(stspButton);
+		
 		swarmPanel = new SwarmPanel();
 		
 		setLayout(new BorderLayout());
@@ -76,18 +126,24 @@ public class Swarm extends JFrame {
 		
 		public void paint(Graphics g) {
 			g.setColor(Color.white);
-			g.fillRect(0, 0, SX, SY);
+			if(cls) {
+				g.fillRect(0, 0, SX, SY);
+			}
 			for(int i=0; i<FLIES; i++) {
 				flies[i].draw(g);
 			}
 		}
 	}
 	
+	public void actionPerformed(ActionEvent e) {
+		
+	}
+	
 	private class Fly {
 		private int id;
 		private int x, y;
 		private int size = 3;
-		private int speed = 5;
+		//private int speed = 5;
 		private Color color = Color.blue;
 		//private Random rand = new Random();
 		
@@ -103,9 +159,6 @@ public class Swarm extends JFrame {
 		public void setColor(Color c) {
 			color = c;
 		}
-		public void setSpeed(int s) {
-			speed = s;
-		}
 		
 		public void setPos(int x, int y) {
 			this.x = x;
@@ -117,7 +170,7 @@ public class Swarm extends JFrame {
 			int dir;
 			while (true) {
 				dir = rand.nextInt((DIAGONALS) ? 8 : 4);
-				System.out.println(dir);
+				//System.out.println(dir);
 				switch (dir) {
 				case 0:
 					dely = -speed;
@@ -148,13 +201,17 @@ public class Swarm extends JFrame {
 					dely = (int) (speed / Math.sqrt(2));
 					break;
 				}
-				System.out.println("(" + delx + ", " + dely + ")");
+				//System.out.println("(" + delx + ", " + dely + ")");
 				if(checkMove(delx, dely)) {
-					System.out.println("(" + delx + ", " + dely + ")");
+					//System.out.println("(" + delx + ", " + dely + ")");
 					x+=delx;
 					y+=dely;
 					return;
 				}
+				return;
+				//x+=delx;
+				//y+=dely;
+				//return;
 			}
 		}
 		
@@ -162,7 +219,7 @@ public class Swarm extends JFrame {
 			for(int i=0; i<FLIES; i++) {
 				if(i==id) continue;
 				if(Math.abs((x+delx)-flies[i].getX())<ADIST && Math.abs((y+dely)-flies[i].getY())<ADIST) {
-					System.out.println("flies[" + id + "]: #" + i + " is too close");
+					//System.out.println("flies[" + id + "]: #" + i + " is too close");
 					return false;
 				}
 			}
